4312eacc80329a5001fecb81a9a20ee46ff16a98
--replace-all <shivamthakkar36@gmail.com>
2024-12-03 18:25:01 -0500

    Update header file for Analytics Class

--- cugl/include/cugl/netcode/CUAnalytics.h
+++ cugl/include/cugl/netcode/CUAnalytics.h
@@ -9,24 +9,33 @@
 
 namespace cugl
 {
+    /**
+     * The classes to support CUGL networking.
+     *
+     * Currently CUGL supports ad-hoc game lobbies using web-sockets. The
+     * sockets must connect connect to a CUGL game lobby server. However,
+     * the actual network layer is supported by high speed WebRTC. See
+     *
+     *     https://libdatachannel.org
+     *
+     * for an explanation of our networking layer.
+     */
     namespace netcode
     {
+
         namespace analytics
         {
-
-            class User
-            {
-            public:
-                User(const std::string &vendorId);
-                std::string getVendorId() const;
-
-            private:
-                std::string _vendorId;
-            };
-
             class Task
             {
+            private:
+                std::string name;
+                // Make a UUID here that will be the task ID
+            };
+
+            class TaskAttempt
+            {
             public:
+                // Keep a unique ID for the task attempt ID
                 enum class Status
                 {
                     COMPLETED,
@@ -36,6 +45,8 @@ namespace cugl
                     NOT_STARTED
                 };
 
+                void setTaskStatus(Status status);
+
                 struct Statistics
                 {
                     std::unordered_map<std::string, int> intStatistics;
@@ -43,17 +54,15 @@ namespace cugl
                     std::unordered_map<std::string, bool> booleanStatistics;
                 };
 
-                Task(const std::string &name);
-                Task(const std::string &name, const std::shared_ptr<Statistics> &taskStatistics);
+                TaskAttempt(const std::shared_ptr<Task> task);
 
-                void setTaskStatus(Status status);
                 void setNumFailures(int num);
                 int getNumFailures() const;
                 bool getIsActive() const;
                 std::string getStartTime() const;
                 std::string getEndTime() const;
                 Status getStatus() const;
-                std::shared_ptr<Statistics> getStatistics() const;
+                void setStatistics() const;
 
             private:
                 std::string _name;
@@ -65,38 +74,61 @@ namespace cugl
                 std::string _endTime;
             };
 
-            class Session
-            {
-            public:
-                bool isCurrentSession() const;
-                void addTask(const std::shared_ptr<Task> &task);
-                const std::vector<std::shared_ptr<Task>> &getTasks() const;
-
-            private:
-                std::vector<std::shared_ptr<Task>> _tasks;
-                bool _isCurrentSession;
-            };
-
             class AnalyticsConnection
             {
+
+#pragma mark Constructors
             public:
-                AnalyticsConnection(const std::string &url); // Get all of the info for vendor id and user detail
-                void init();                                 // This will have the connect part as well.
-                // void connect(); // Connect to the WebSocket
-                std::shared_ptr<User> getUser();
-                // void startSession();
-                // void endSession();
-                // std::shared_ptr<Session> getCurrentSession();
-                // std::shared_ptr<Session> getLastEndedSession();
-                void defineTask(const std::shared_ptr<Task> &task);
-                void addTaskAttempt(const std::shared_ptr<Task> &task);
+                AnalyticsConnection();
+                ~AnalyticsConnection();
+
+            private:
+                void dispose();
+                bool init(const InetAddress &address); // Make sure you init with our hardcoded path
+
+#pragma mark Communication
+            public:
+                void open(bool secure = false);
+
+                void close();
+
+#pragma mark Static Allocators
+
+            public:
+                static std::shared_ptr<AnalyticsConnection> alloc(const InetAddress &address)
+                {
+                    std::shared_ptr<AnalyticsConnection> result = std::make_shared<AnalyticsConnection>();
+                    return (result->init(address) ? result : nullptr);
+                }
+
+#pragma mark Debugging
+                /**
+                 * Toggles the debugging status of this connection.
+                 *
+                 * If debugging is active, connections will be quite verbose
+                 *
+                 * @param flag  Whether to activate debugging
+                 */
+                void setDebug(bool flag);
+
+                /**
+                 * Returns the debugging status of this connection.
+                 *
+                 * If debugging is active, connections will be quite verbose
+                 *
+                 * @return the debugging status of this connection.
+                 */
+                bool getDebug();
+
+                void addTask(const std::shared_ptr<Task> &task);
+                void addTasks(const std::vector<std::shared_ptr<Task>> &tasks);
+                void addTaskAttempt(const std::shared_ptr<TaskAttempt> &TaskAttempt);
                 // void endTask(const std::shared_ptr<Task> &task);
-                void syncTaskAttempt(const std::shared_ptr<Task> &task);
-                void recordAction(const std::shared_ptr<Task> &task); // Add Action Data here
+                void syncTaskAttempt(const std::shared_ptr<TaskAttempt> &TaskAttempt);
+                void recordAction(const cugl::JsonValue actionBlob); // Add Action Data here
 
             private:
                 std::shared_ptr<WebSocket> _webSocket;
-                std::shared_ptr<User> _user;
             };
         }
     } // namespace netcode
